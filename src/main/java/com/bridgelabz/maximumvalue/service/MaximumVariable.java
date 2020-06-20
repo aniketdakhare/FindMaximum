2701fa7998d1f092196219663c4b23b887db196c
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-20 16:08:37 +0530

    ADDED method to find maximum value

--- src/main/java/com/bridgelabz/maximumvalue/service/MaximumVariable.java
+++ src/main/java/com/bridgelabz/maximumvalue/service/MaximumVariable.java
@@ -3,8 +3,13 @@ package com.bridgelabz.maximumvalue.service;
 public class MaximumVariable
 {
 
-    public String welcomeMassage(String massage)
+    public int maximumValue(Integer firstNumber, Integer secondNumber, Integer thirdNumber)
     {
-        return "Welcome";
+        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0)
+            return firstNumber;
+        else if (secondNumber.compareTo(thirdNumber) > 0 && secondNumber.compareTo(firstNumber) > 0)
+            return secondNumber;
+        else
+            return thirdNumber;
     }
 }
