aee5befb7bec4b9d6fd6ac6adae29ca5e29a0f3a
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-20 17:13:57 +0530

    ADDED method to get maximum Float value

--- src/main/java/com/bridgelabz/maximumvalue/service/MaximumVariable.java
+++ src/main/java/com/bridgelabz/maximumvalue/service/MaximumVariable.java
@@ -2,8 +2,18 @@ package com.bridgelabz.maximumvalue.service;
 
 public class MaximumVariable
 {
+    public Integer maximumIntegerValue(Integer firstNumber, Integer secondNumber, Integer thirdNumber)
+    {
+        if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0)
+            return firstNumber;
+        else if (secondNumber.compareTo(thirdNumber) > 0 && secondNumber.compareTo(firstNumber) > 0)
+            return secondNumber;
+        else
+            return thirdNumber;
+    }
 
-    public int maximumValue(Integer firstNumber, Integer secondNumber, Integer thirdNumber)
+
+    public Float maximumFloatValue(Float firstNumber, Float secondNumber, Float thirdNumber)
     {
         if (firstNumber.compareTo(secondNumber) > 0 && firstNumber.compareTo(thirdNumber) > 0)
             return firstNumber;
