2eab16db5f650f437fbb7d347bf27dc2beab9da4
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-20 16:10:03 +0530

    ADDED test method maximum number at first position

--- src/test/java/com/bridgelabz/testmaximumvalue/TestMaximumVariable.java
+++ src/test/java/com/bridgelabz/testmaximumvalue/TestMaximumVariable.java
@@ -2,15 +2,23 @@ package com.bridgelabz.testmaximumvalue;
 
 import com.bridgelabz.maximumvalue.service.MaximumVariable;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class TestMaximumVariable 
 {
-    @Test
-    public void testWelcomeMasage()
+    MaximumVariable maximumVariable;
+
+    @Before
+    public void createObjectOf_MaximumVariableClass()
     {
-        MaximumVariable maximumVariable = new MaximumVariable();
-        String massage = maximumVariable.welcomeMassage("Welcome to find maximum value");
-        Assert.assertEquals("Welcome", massage);
+        maximumVariable = new MaximumVariable();
+    }
+
+    @Test
+    public void givenMaximumNumberAt_FirstPosition_ReturnsSameNumber()
+    {
+        Integer maximum = maximumVariable.maximumValue(88, 44, 67);
+        Assert.assertEquals((Integer) 88, maximum);
     }
 }
