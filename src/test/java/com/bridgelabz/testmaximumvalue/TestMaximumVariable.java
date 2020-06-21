69b01241fcadf9ce0a0c51792261ab78867f9bc9
“Aniket_D” <aniketdakhare@gmail.com>
2020-06-22 00:10:07 +0530

    REFACTORED test cases for multiple inputs

--- src/test/java/com/bridgelabz/testmaximumvalue/TestMaximumVariable.java
+++ src/test/java/com/bridgelabz/testmaximumvalue/TestMaximumVariable.java
@@ -3,69 +3,28 @@ package com.bridgelabz.testmaximumvalue;
 import com.bridgelabz.maximumvalue.service.MaximumVariable;
 import org.junit.Assert;
 import org.junit.Test;
+import java.util.Arrays;
 
 public class TestMaximumVariable 
 {
     @Test
     public void givenMaximumIntegerValueAt_FirstPosition_ReturnsSameNumber()
     {
-        Integer maximum = new MaximumVariable<>(88, 44, 67).maximumValue();
-        Assert.assertEquals((Integer) 88, maximum);
-    }
-
-    @Test
-    public void givenMaximumIntegerValueAt_SecondPosition_ReturnsSameNumber()
-    {
-        Integer maximum = new MaximumVariable<>(88, 99, 67).maximumValue();
-        Assert.assertEquals((Integer) 99, maximum);
-    }
-
-    @Test
-    public void givenMaximumIntegerValueAt_ThirdPosition_ReturnsSameNumber()
-    {
-        Integer maximum = new MaximumVariable<>(88, 44, 99).maximumValue();
-        Assert.assertEquals((Integer) 99, maximum);
+       Integer maximum = new MaximumVariable<>(Arrays.asList(34,56,22,4,67,90,95,2)).maximumValue();
+        Assert.assertEquals((Integer) 95, maximum);
     }
 
     @Test
     public void givenMaximumFloatValueAt_FirstPosition_ReturnsSameValue()
     {
-        Float maximum = new MaximumVariable<>(8.8f, 4.4f, 6.7f).maximumValue();
-        Assert.assertEquals((Float) 8.8f, maximum);
-    }
-
-    @Test
-    public void givenMaximumFloatValueAt_SecondPosition_ReturnsSameValue()
-    {
-        Float maximum = new MaximumVariable<>(2.8f, 8.4f, 6.7f).maximumValue();
-        Assert.assertEquals((Float) 8.4f, maximum);
-    }
-
-    @Test
-    public void givenMaximumFloatValueAt_ThirdPosition_ReturnsSameValue()
-    {
-        Float maximum = new MaximumVariable<>(8.8f, 4.4f, 9.9f).maximumValue();
+        Float maximum = new MaximumVariable<>(Arrays.asList(2.4f,5.6f,0.4f,7.7f,4.5f,9.9f)).maximumValue();
         Assert.assertEquals((Float) 9.9f, maximum);
     }
 
     @Test
     public void givenMaximumStringValueAt_FirstPosition_ReturnsSameValue()
     {
-        String maximum = new MaximumVariable<>("Peach", "Apple", "Banana").maximumValue();
-        Assert.assertEquals("Peach", maximum);
-    }
-
-    @Test
-    public void givenMaximumStringValueAt_SecondPosition_ReturnsSameValue()
-    {
-        String maximum = new MaximumVariable<>("Apple", "Peach", "Banana").maximumValue();
-        Assert.assertEquals("Peach", maximum);
-    }
-
-    @Test
-    public void givenMaximumStringValueAt_ThirdPosition_ReturnsSameValue()
-    {
-        String maximum = new MaximumVariable<>("Apple", "Banana", "Peach").maximumValue();
-        Assert.assertEquals("Peach", maximum);
+        String maximum = new MaximumVariable<>(Arrays.asList("Apple","Peach","Banana","Horse","Snake")).maximumValue();
+        Assert.assertEquals("Snake", maximum);
     }
 }
